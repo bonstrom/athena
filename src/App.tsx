08407542b1e18813a51cb45c93ee29b2bb95b333
Daniel Bönström <daniel.bonstrom@cab.se>
2026-04-04 14:08:12 +0200

    fix performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import ChatLayout from "./components/ChatLayout";
-import { GlobalErrorSnackbar } from "./components/GlobalErrorSnackbar";
-import ChatView from "./pages/ChatView";
-import Home from "./pages/Home";
-import Settings from "./pages/Settings";
-import { useAutoBackup } from "./hooks/useAutoBackup";
-import { useAuthStore } from "./store/AuthStore";
-import { ErrorBoundary } from "./components/ErrorBoundary";
+import React, { Suspense } from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import ChatLayout from './components/ChatLayout';
+import { GlobalErrorSnackbar } from './components/GlobalErrorSnackbar';
+import { useAutoBackup } from './hooks/useAutoBackup';
+import { useAuthStore } from './store/AuthStore';
+import { ErrorBoundary } from './components/ErrorBoundary';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Settings = React.lazy(() => import('./pages/Settings'));
+const ChatView = React.lazy(() => import('./pages/ChatView'));
 
 const App: React.FC = () => {
   const { backupInterval } = useAuthStore();
@@ -15,26 +17,17 @@ const App: React.FC = () => {
   return (
     <ErrorBoundary>
       <HashRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<ChatLayout />}>
-            <Route
-              index
-              element={<Home />}
-            />
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<ChatLayout />}>
+              <Route index element={<Home />} />
 
-            <Route
-              path="settings"
-              element={<Settings />}
-            />
+              <Route path="settings" element={<Settings />} />
 
-            <Route
-              path="chat/:topicId"
-              element={<ChatView />}
-            />
-          </Route>
-        </Routes>
+              <Route path="chat/:topicId" element={<ChatView />} />
+            </Route>
+          </Routes>
+        </Suspense>
 
         <GlobalErrorSnackbar />
       </HashRouter>
